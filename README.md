cd60ae18ca2dd8838f5461a5094aaeff108bbd1c
Nicolas Gamba <nicolasgamba.lp@gmail.com>
2024-04-14 00:39:07 -0300

    negrita cocina prueba pedidos

---
+++ README.md
@@ -0,0 +1,1 @@
+# negritacocina.github.io
---
+++ script.js
@@ -0,0 +1,48 @@
+let textoDelPedido = "Hola!\nQuería realizar el siguiente pedido:\n\n";
+let pedido = {};
+
+function createItem(id) {
+    let name = document.getElementById(id).getElementsByTagName("h2")[0].textContent;
+    return {
+        cant: 0,
+        name
+    }
+}
+
+function agregar(id) {
+    if (!pedido.hasOwnProperty(id)) pedido[id] = createItem(id);
+    pedido[id].cant++;
+    actualizarCant(id)
+}
+
+function quitar(id) {
+    if (pedido.hasOwnProperty(id) && pedido[id].cant > 0) {
+        pedido[id].cant--;
+        actualizarCant(id)
+    }
+}
+
+function actualizarCant(id) {
+    let cant = document.getElementById(id).getElementsByClassName('cant')[0];
+    cant.innerHTML = pedido[id].cant;
+}
+
+function sanitizarPedido() {
+    let keys = Object.keys(pedido);
+    keys.forEach(key => {
+        if (pedido[key].cant == 0) delete pedido[key];
+    });
+}
+
+function generarTexto() {
+    let items = Object.keys(pedido);
+    items.forEach(item => {
+        textoDelPedido += `${pedido[item].name}: ${pedido[item].cant}\n`;
+    });
+}
+
+function hacerPedido() {
+    sanitizarPedido();
+    generarTexto();
+    window.location.href = "https://wa.me/5492213640726?text=" + encodeURIComponent(textoDelPedido);
+}
